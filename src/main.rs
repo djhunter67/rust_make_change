3a753d6cce36c8f5cc865e98788a7be09bc8f4b4
Christerpher Hunter <djhunter67@gmail.com>
2022-05-24 22:51:16 -0400

    completed making struct and impl method; need to work out logic

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,31 @@
 mod make_change;
 
+use std::io;
+use make_change::Coins;
+use math::round;
+
 fn main() {
-    println!("Hello, world!");
+    let coin_choice: Coins = Coins {
+        pennies: 1,
+        nickels: 5,
+        dimes: 10,
+        quarters: 25,
+    };
+
+    println!("Please enter a dollar and change amount: ");
+    let mut user_input: String = String::new();
+
+    io::stdin()
+    .read_line(&mut user_input)
+    .expect("Failed to read line");
+
+    let user_input: f32 = match user_input.trim().parse() {
+        Ok(num) => num,
+        Err(_) => println!("Error, incorrect input"),
+        };
+        
+    let correct_change: u32 = coin_choice.make_coins(user_input);
+
+    
+
 }
--- src/make_change.rs
+++ src/make_change.rs
@@ -1,9 +1,15 @@
 // Make change
 
 pub struct Coins {
-    pennies: u32,
-    nickels: u32,
-    dimes: u32,
-    quarters: u32,
+    pub pennies: u32,
+    pub nickels: u32,
+    pub dimes: u32,
+    pub quarters: u32,
 }
 
+impl Coins {
+    pub fn make_coins(&self, change: f32) -> u32 {
+        const DOLLAR_MAKER: u32 = 100;
+        change as u32 * DOLLAR_MAKER 
+    }
+}
