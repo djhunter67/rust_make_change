ad025f93848f4c2aebfbe940a2c729db33e3877f
Christerpher Hunter <djhunter67@gmail.com>
2022-05-26 21:00:11 -0400

    correct change given; no input validation

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,7 @@
 mod make_change;
 
-use std::io;
 use make_change::Coins;
-use math::round;
+use std::io;
 
 fn main() {
     let coin_choice: Coins = Coins {
@@ -16,16 +15,26 @@ fn main() {
     let mut user_input: String = String::new();
 
     io::stdin()
-    .read_line(&mut user_input)
-    .expect("Failed to read line");
+        .read_line(&mut user_input)
+        .expect("Failed to read line");
 
     let user_input: f32 = match user_input.trim().parse() {
         Ok(num) => num,
-        Err(_) => println!("Error, incorrect input"),
-        };
-        
-    let correct_change: u32 = coin_choice.make_coins(user_input);
+        Err(_) => 0.0,
+    };
 
-    
+    let mut rem_change: u32 = coin_choice.make_int(user_input);
+    let quarter_count: u32 = coin_choice.make_quarters(rem_change);
+    rem_change = coin_choice.after_quarters(rem_change, quarter_count);
+    let dime_cnt: u32 = coin_choice.make_dimes(rem_change);
+    rem_change = coin_choice.after_dimes(rem_change, dime_cnt);
+    let nickel_cnt = coin_choice.make_nickels(rem_change);
+    rem_change = coin_choice.after_nickels(rem_change, nickel_cnt);
+    let penny_cnt = coin_choice.make_pennies(rem_change);
+
+    println!("\nQuarters: {}", quarter_count);
+    println!("Dimes: {}", dime_cnt);
+    println!("Nickels: {}", nickel_cnt);
+    println!("Pennies: {}", penny_cnt);
 
 }
--- src/make_change.rs
+++ src/make_change.rs
@@ -8,8 +8,44 @@ pub struct Coins {
 }
 
 impl Coins {
-    pub fn make_coins(&self, change: f32) -> u32 {
-        const DOLLAR_MAKER: u32 = 100;
-        change as u32 * DOLLAR_MAKER 
+    pub fn make_int(&self, input: f32) -> u32 {
+        const DOLLAR_MAKER: f32 = 100.;
+        let coin_amount: f32 = input * DOLLAR_MAKER;
+        coin_amount as u32
+    }
+
+    pub fn make_quarters(&self, change: u32) -> u32 {
+        const one_quarter: u32 = 25;
+        &change / &one_quarter
+    }
+
+    pub fn after_quarters(&self, change: u32, num_of_quarters: u32) -> u32 {
+        const one_quarter: u32 = 25;
+        &change - &num_of_quarters * &one_quarter
+    }
+
+    pub fn make_dimes(&self, rem_change: u32) -> u32 {
+        const one_dime: u32 = 10;
+        &rem_change / &one_dime
+    }
+
+    pub fn after_dimes(&self, rem_change: u32, num_of_dimes: u32) -> u32 {
+        const one_dime: u32 = 10;
+        &rem_change - &num_of_dimes * &one_dime
+    }
+
+    pub fn make_nickels(&self, rem_change: u32) -> u32 {
+        const one_nickel: u32 = 5;
+        &rem_change / &one_nickel
+    }
+
+    pub fn after_nickels(&self, rem_change: u32, num_of_nickels: u32) -> u32 {
+        const one_nickel: u32 = 5;
+        &rem_change - &num_of_nickels * &one_nickel
+    }
+
+    pub fn make_pennies(&self, rem_change: u32) -> u32 {
+        const one_penny: u32 = 1;
+        &rem_change / &one_penny
     }
 }
